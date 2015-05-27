77d3770c3ee55ead7a00951b9cd52e3f98a49ea0
Takuya Kuwahara <taakuu19@gmail.com>
2015-05-27 21:34:44 +0900

    Catch Exception in job1-reducer

--- src/mapreduce/job1/reducer.py
+++ src/mapreduce/job1/reducer.py
@@ -7,21 +7,26 @@ def main():
     links_dict = {}
     # Input
     for line in sys.stdin:
-        page_id, which, title = line.strip().split('\t')
-        if which == 'p':
-            id_to_title[page_id] = title
-        elif which == 'l':
-            if not page_id in links_dict:
-                links_dict[page_id] = []
-            links_dict[page_id].append(title)
+        try:
+            page_id, which, title = line.strip().split('\t')
+            if which == 'p':
+                id_to_title[page_id] = title
+            elif which == 'l':
+                if not page_id in links_dict:
+                    links_dict[page_id] = []
+                links_dict[page_id].append(title)
+        except Exception:
+            continue
 
     # Output
     for page_id, title in id_to_title.items():
-        if not page_id in links_dict:
-            print("{0}\t1.0".format(title))
+        try:
+            if not page_id in links_dict:
+                print("{0}\t1.0".format(title))
+                continue
+            print("{0}\t1.0\t{1}".format(title, "\t".join(links_dict[page_id])))
+        except Exception:
             continue
-        print("{0}\t1.0\t{1}".format(title, "\t".join(links_dict[page_id])))
-
 
 if __name__ == '__main__':
     main()
