cbf0092c4dfc9d0afd0c591c1810cabcddd83b86
Takuya Kuwahara <taakuu19@gmail.com>
2015-05-27 22:08:47 +0900

    Catch exception in job2-mapper-reducer

--- src/mapreduce/job2/mapper.py
+++ src/mapreduce/job2/mapper.py
@@ -6,14 +6,26 @@ PAGE_INDEX = 0
 RANK_INDEX = 1
 LINKS_INDEX = 2
 
-for line in sys.stdin:
-    row = line.strip().split('\t')
-    page, rank = row[PAGE_INDEX], row[RANK_INDEX]
-    print("{0}\t!".format(page))
-    if len(row) == 2:
-        continue
-    links = row[LINKS_INDEX:]
-    total_links = len(links)
-    for other in links:
-        print("{0}\t{1}\t{2}\t{3}".format(other, page, rank, total_links))
-    print("{0}\t|{1}".format(page, "\t".join(links)))
+def main():
+    while True:
+        try:
+            line = sys.stdin.readline()
+            if not line:
+                break
+            row = line.strip().split('\t')
+            page, rank = row[PAGE_INDEX], row[RANK_INDEX]
+            print("{0}\t!".format(page))
+            if len(row) == 2:
+                continue
+            links = row[LINKS_INDEX:]
+            total_links = len(links)
+            for other in links:
+                print("{0}\t{1}\t{2}\t{3}".format(other, page, rank, total_links))
+            print("{0}\t|{1}".format(page, "\t".join(links)))
+
+        except Exception:
+            continue
+
+if __name__ == '__main__':
+    main()
+
--- src/mapreduce/job2/reducer.py
+++ src/mapreduce/job2/reducer.py
@@ -4,32 +4,42 @@ import sys
 
 DAMPING = 0.85
 
-pages_dict = {}
-
-for line in sys.stdin:
-    splited = line.strip().split('\t')
-    if not splited[0] in pages_dict:
-        pages_dict[splited[0]] = []
-    pages_dict[splited[0]].append(splited[1:])
-
-for key, values in pages_dict.items():
-    is_exist = False
-    links = ""
-    sum_ranks = 0.0
-
-    for page in values:
-        if page[0] == "!":
-            is_exist = True
+def main():
+    pages_dict = {}
+    while True:
+        try:
+            line = sys.stdin.readline()
+            if not line:
+                break
+            splited = line.strip().split('\t')
+            if not splited[0] in pages_dict:
+                pages_dict[splited[0]] = []
+            pages_dict[splited[0]].append(splited[1:])
+        except Exception:
             continue
-        if page[0].startswith("|"):
-            page[0] = page[0][1:]
-            links = "\t" + "\t".join(page)
-            continue
-        rank = float(page[1])
-        count_outlinks = int(page[2])
-        sum_ranks += (rank / count_outlinks)
+    
+    for key, values in pages_dict.items():
+        is_exist = False
+        links = ""
+        sum_ranks = 0.0
 
-    if not is_exist:
-        continue
-    new_rank = DAMPING * sum_ranks + (1.0 - DAMPING)
-    print("{0}\t{1}{2}".format(key, new_rank, links))
+        for page in values:
+            if page[0] == "!":
+                is_exist = True
+                continue
+            if page[0].startswith("|"):
+                page[0] = page[0][1:]
+                links = "\t" + "\t".join(page)
+                continue
+            rank = float(page[1])
+            count_outlinks = int(page[2])
+            sum_ranks += (rank / count_outlinks)
+
+        if not is_exist:
+            continue
+        new_rank = DAMPING * sum_ranks + (1.0 - DAMPING)
+        print("{0}\t{1}{2}".format(key, new_rank, links))
+
+if __name__ == '__main__':
+    main()
+
